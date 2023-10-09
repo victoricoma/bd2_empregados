d198583cfdbb3809492b02a21571cdce67c2b03a
victoricoma <victor.icoma@etec.sp.gov.br>
2023-10-09 20:30:13 -0300

    Aula0910 Filtros condicionais

---
+++ Aula0910/CONDITIONAL_FILTER.sql
@@ -0,0 +1,20 @@
+select
+	nome as empregado,
+    salario as renda,
+    if(salario<=2112,"Isento",
+		if(salario>2112 and salario<=2826.85,"Paga 7,5%",
+			if(salario>2826.85 and salario<=3751.05,"Paga 15%",
+				if(salario>3751.05 and salario<=4664.68,"Paga 22,5%","Paga 27,5%")
+			)
+		)
+    ) as IRPF,
+        if(salario<=2112,0,
+		if(salario>2112 and salario<=2826.85,format((salario * 13.3)*0.075,2),
+			if(salario>2826.85 and salario<=3751.05,format((salario * 13.3)*0.15,2),
+				if(salario>3751.05 and salario<=4664.68,format((salario * 13.3)*0.225,2),
+                format((salario * 13.3)*0.275,2))
+			)
+		)
+    ) as Tributo
+from
+	empregado;
